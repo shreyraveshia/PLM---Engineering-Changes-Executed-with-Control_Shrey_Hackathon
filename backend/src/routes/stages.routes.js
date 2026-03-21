34837adf42969b5414856dbae76fa7f11e02eff0
shreyraveshia <shreyraveshia07@gmail.com>
2026-03-22 04:43:55 +0530

    implemented stages.routes.js

--- backend/src/routes/stages.routes.js
+++ backend/src/routes/stages.routes.js
@@ -1,1 +1,12 @@
-const router = require('express').Router(); module.exports = router;
+const express = require('express');
+const router = express.Router();
+const { getAll, create, update, remove } = require('../controllers/stages.controller');
+const { verifyToken } = require('../middleware/auth');
+const { isAdmin } = require('../middleware/rbac');
+
+router.get('/', verifyToken, getAll);
+router.post('/', verifyToken, isAdmin, create);
+router.put('/:id', verifyToken, isAdmin, update);
+router.delete('/:id', verifyToken, isAdmin, remove);
+
+module.exports = router;
